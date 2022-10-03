bcf3ce2221bd8b6009eacdb6c2650b0c990bd9ec
Ikram Akbar <ikramakbar71@gmail.com>
2022-10-03 17:01:46 +0600

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { serverComponent } from './server/server.component';
 import { NewsComponent } from './news/news.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StudentComponent } from './student/student.component';
 import { NewServerComponent } from './new-server/new-server.component';
 import { FormComponent } from './form/form.component'
@@ -23,6 +23,7 @@ import { FormComponent } from './form/form.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     FormsModule
   ],
   providers: [],
